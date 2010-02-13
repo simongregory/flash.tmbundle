fe94a4a6b0e4e15fb5ef62308aa099d146cd748f
Simon Gregory <simon@helvector.org>
2010-02-13 22:36:14 +0000

    • Now searches for the most recent Flash.app in it's default install location.
    ie Applications/Adobe Flash CS5/Adobe Flash CS5.app

--- Support/lib/flash_app.rb
+++ Support/lib/flash_app.rb
@@ -2,10 +2,39 @@
 
 class FlashApp
   
-  attr_reader :name
-             
-  def initialize()    
-    @name = "Adobe Flash CS3"
+  attr_reader :name, :path
+  
+  def initialize()
+    find_app
   end
+  
+  protected
+  
+  # Find the most recent version of the Flash authoring tool.
+  #
+  def find_app
+    
+    [5,4,3].each { |n|
+      
+      f = "Adobe Flash CS#{n}"
+      p = "/Applications/#{f}/#{f}.app"
+      
+      if File.exist?(p)
+        @name = f
+        @path = p
+        break
+      end
+      
+    }
+    
+  end
+  
+end
+
+if __FILE__ == $0
+  
+  fa = FlashApp.new
+  puts fa.name
+  puts fa.path
   
 end
